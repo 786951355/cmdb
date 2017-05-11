6c1990d1d0a1bfdd040244f40a4043f2d55d4d47
786951355 <786951355@qq.com>
2017-05-11 10:25:38 +0800

    rm some files

--- myperm/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MypermConfig(AppConfig):
-    name = 'myperm'
--- myperm/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def asset_perm(request):
-    pass
